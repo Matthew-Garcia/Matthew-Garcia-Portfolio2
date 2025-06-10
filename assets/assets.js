06631388c3c581e890f8e55852a45956c693ae57
Matthew Garcia <127278603+Matthew-Garcia@users.noreply.github.com>
2025-06-10 15:51:48 -0500

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -78,8 +78,8 @@ export const assets = {
 
 export const workData = [
     {
-        title: 'Frontend project',
-        description: 'Web Design',
+        title: 'SCARA Robot Arm',
+        description: 'Embedded Systems &vRobotics',
         bgImage: '/work-1.png',
     },
     {
@@ -108,7 +108,7 @@ export const serviceData = [
 
 export const infoList = [
     { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.S. in Computer Engineering (in progress), A.S. in Pre-Engineering & General Studies' },    
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
 
