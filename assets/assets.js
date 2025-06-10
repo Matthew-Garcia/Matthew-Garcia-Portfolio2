694e3572b4a4d3283296e0a3750747edfa236980
Matthew Garcia <127278603+Matthew-Garcia@users.noreply.github.com>
2025-06-10 16:15:27 -0500

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -107,7 +107,7 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'C/C++, Python, JavaScript, Verilog, VHDL, ARM Assembly, Bash, HTML/CSS, ROS2, SQL' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.S. in Computer Engineering (expected Jan 2026), A.S. in Pre-Engineering, A.S. in General Studies' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
